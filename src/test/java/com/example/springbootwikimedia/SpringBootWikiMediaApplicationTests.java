f667dcc131ad4a45ac35ca52de1a7c22fe0189d5
Shivakant6387 <shivakant5875@gmail.com>
2023-06-02 18:44:33 +0530

    kafka topic and producer and handler

---
+++ src/test/java/com/example/springbootwikimedia/SpringBootWikiMediaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootwikimedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWikiMediaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
